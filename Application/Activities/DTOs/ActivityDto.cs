e7111d364c1e4483fc8143499fbab740a9b0b66d
James Hollins <jhollins@pdinet.com>
2026-05-25 20:34:32 +0100

    End of Section 14

--- Application/Activities/DTOs/ActivityDto.cs
+++ Application/Activities/DTOs/ActivityDto.cs
@@ -1,10 +1,11 @@
 using System;
+using Application.Profiles.DTOs;
 
-namespace Domain;
+namespace Application.Activities.DTOs;
 
-public class Activity
+public class ActivityDto
 {
-    public string Id { get; set; } = Guid.NewGuid().ToString();
+    public required string Id { get; set; }
 
     public required String Title { get; set; }
 
@@ -16,6 +17,10 @@ public class Activity
 
     public bool IsCancelled { get; set; }
 
+    public required string HostDisplayName { get; set; }
+
+    public required string HostId { get; set; }
+
     //location props
     public required String City { get; set; }
 
@@ -26,5 +31,5 @@ public class Activity
     public double Longitude { get; set; }
 
     // navigation properties
-    public ICollection<ActivityAttendee> Attendees { get; set; } = [];
+    public ICollection<UserProfile> Attendees { get; set; } = [];
 }
--- Domain/User.cs
+++ Domain/User.cs
@@ -10,4 +10,7 @@ public class User : IdentityUser
     public string? Bio { get; set; }
 
     public string? ImageUrl { get; set; }
+
+    //navigation properties
+    public ICollection<ActivityAttendee> Activities { get; set; } = [];
 }
